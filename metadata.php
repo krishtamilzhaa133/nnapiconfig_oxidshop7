6a80232c722a0cc7b594e5382ffa5192a79a1385
krishtamilzhaa133 <134515146+krishtamilzhaa133@users.noreply.github.com>
2023-11-21 12:51:44 +0530

    Update metadata.php

--- metadata.php
+++ metadata.php
@@ -28,11 +28,11 @@ $aModule = [
 
 
         'settings'      => [
-            ['group' => 'nninvoiceapiconfiguration', 'name' => 'paymentaccesskey','type' => 'str',   'value'  => '', 'position' => 1 ],
-            ['group' => 'nninvoiceapiconfiguration', 'name' => 'productactivationkey',    'type' => 'str',   'value'  => '', 'position' => 2 ],
-            ['group' => 'nninvoiceapiconfiguration', 'name' => 'traiffid',    'type' => 'str',   'value'  => '', 'position' => 3 ],
-            ['group' => 'nninvoiceapiconfiguration', 'name' => 'clientkey',    'type' => 'str',   'value'  => '', 'position' => 4 ],
-            ['group' => 'nninvoiceapiconfiguration', 'name' => 'projectid',    'type' => 'str',   'value'  => '', 'position' => 5 ],
+            ['group' => 'nnapiconfiguration', 'name' => 'paymentaccesskey','type' => 'str',   'value'  => '', 'position' => 1 ],
+            ['group' => 'nnapiconfiguration', 'name' => 'productactivationkey',    'type' => 'str',   'value'  => '', 'position' => 2 ],
+            ['group' => 'nnapiconfiguration', 'name' => 'traiffid',    'type' => 'str',   'value'  => '', 'position' => 3 ],
+            ['group' => 'nnapiconfiguration', 'name' => 'clientkey',    'type' => 'str',   'value'  => '', 'position' => 4 ],
+            ['group' => 'nnapiconfiguration', 'name' => 'projectid',    'type' => 'str',   'value'  => '', 'position' => 5 ],
             
             
         ],
