4d9e57ce007bb8ee2ef8aba230bebfd4e00f98f4
krishtamilzhaa133 <134515146+krishtamilzhaa133@users.noreply.github.com>
2023-11-21 12:52:28 +0530

    Update nnapiconfigure_lang.php

--- views/admin_twig/en/nnapiconfigure_lang.php
+++ views/admin_twig/en/nnapiconfigure_lang.php
@@ -7,7 +7,7 @@ $sLangName = 'English';
 $aLang = array(
 
     'charset' => 'UTF-8',
-    'SHOP_MODULE_GROUP_nninvoiceapiconfiguration' => 'Novalnet Invoice API Configuration',
+    'SHOP_MODULE_GROUP_nnapiconfiguration' => 'Novalnet Invoice API Configuration',
     'SHOP_MODULE_paymentaccesskey' => 'Payment Access Key',
     'SHOP_MODULE_productactivationkey' => 'Product Activation Key',
     'SHOP_MODULE_traiffid' => 'Traiff Id',
